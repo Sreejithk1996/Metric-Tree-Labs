ad79c8c833db1e5d21a74a78aa01f4780a68982e
Sreejithk1996 <sreekumar1996@gmail.com>
2024-03-01 15:19:43 +0530

    project section completed

--- src/components/process-section/Process.jsx
+++ src/components/process-section/Process.jsx
@@ -14,23 +14,43 @@ function Process() {
             <div className='project-1'>
                 <div className='clinic'>
                     <img src={clinictopics} alt='img'></img>
+                    <div className='details-1 details'>
+                        <h6>CLINICTOPICS</h6>
+                        <p>React Native, Django, React.JS</p>
+                    </div>
                 </div>
                 <div className='ourprojects'>
                     <p>Our</p><label>Projects</label>
                 </div>
                 <div className='bim'>
                     <img src={bimex} alt='img'></img>
+                    <div className='details-2 details'>
+                        <h6>BIMEX</h6>
+                        <p>React Native, Django, React.JS</p>
+                    </div>
                 </div>
             </div>
             <div className='project-2'>
                 <div className='sit'>
                     <img src={sitrex} alt='img'></img>
+                    <div className='details-3 details'>
+                        <h6>SITREX</h6>
+                        <p>React Native, Django, React.JS</p>
+                    </div>
                 </div>
                 <div className='yal'>
                     <img src={yalla} alt='img'></img>
+                    <div className='details-4 yal-style'>
+                        <h6>YALLATOYS</h6>
+                        <p>React Native, Django, React.JS</p>
+                    </div>
                 </div>
                 <div className='mag'>
                     <img src={magic} alt='img'></img>
+                    <div className='details-5 details'>
+                        <h6>MAGICREWARDS</h6>
+                        <p>React Native, Django, React.JS</p>
+                    </div>
                 </div>
             </div>
         </section>
