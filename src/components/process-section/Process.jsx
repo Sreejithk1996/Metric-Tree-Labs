ca84aa7a0e455972bb208239ac3e49cb36b46d70
Sreejithk1996 <sreekumar1996@gmail.com>
2024-03-04 18:22:04 +0530

    elements added

--- src/components/process-section/Process.jsx
+++ src/components/process-section/Process.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './process.css'
+import mobiletop from '../../assets/small_image/mob-bg-top.svg'
+import homebar from '../../assets/small_image/True.svg'
 
 function Process() {
   return (
@@ -12,18 +14,27 @@ function Process() {
                 <div className='process-title-content'>
                     <p>Superior processes, world-class user experience designs, rapid application development, and the best after-deployment services are the marks of the best mobile app development practices in place and we are proud to be one of the top mobile app development companies in business today. </p>
                 </div>
-                <button>Start your project<i class="fa-solid fa-arrow-right" style={{color: '#ffffff'}} /></button>
+                <div className='process-title-button'>
+                        <button>Start your project<i class="fa-solid fa-arrow-right" style={{color: '#ffffff'}} /></button>
+                </div>
             </div>
             <div className='pc-2'>
                 <div className='mobile-process-list'>
                     <div className='mobile-topbar'>
-
+                        <img src={mobiletop} alt='mobiletop'></img>
                     </div>
                     <div className='mobile-list'>
-
+                        <ul className='list-content'>
+                            <li className='list-1'><p>01</p><p>Analyze</p></li>
+                            <li className='list-1'><p>02</p><p>Design</p></li>
+                            <li className='list-1'><p>03</p><p>Develop</p></li>
+                            <li className='list-1'><p>04</p><p>Test</p></li>
+                            <li className='list-1'><p>05</p><p>Deploy</p></li>
+                            <li className='list-1'><p>06</p><p>Support</p></li>
+                        </ul>
                     </div>
                     <div className='bottom-bar'>
-                        
+                        <img src={homebar} alt="homebar" />
                     </div>
                 </div>
             </div>
